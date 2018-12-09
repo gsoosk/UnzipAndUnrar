cf44e1c5ad206275ee66d26459ab41386ba27584
Farzad Habibi <farzadhabibii98@gmail.com>
2018-12-09 20:54:50 +0330

    adding optional remove rar and zip files

--- unzipAndUnrar.sh
+++ unzipAndUnrar.sh
@@ -1,10 +1,21 @@
+DEL="false"
+for opt in $* ; do
+    if [ "$opt" = "-r" ]; then
+        DEL="true"
+    fi
+done 
+
 find $1 -name "*.rar" | while read filename
 do 
     unrar x -y "$filename" "`dirname "$filename"`"
-    rm "$filename"
+    if [ $DEL = "true" ]; then
+        rm "$filename"
+    fi
 done 
 find $1 -name "*zip" | while read filename
 do 
     unzip -o -d "`dirname "$filename"`" "$filename"
-    rm "$filename"
+    if [ $DEL = "true" ]; then
+        rm "$filename"
+    fi
 done 
